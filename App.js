d9b1a0bd8ea0ce8ca69ff7b264294b8ca9b96599
shawndevp <shawn.ornerstedt99@hotmail.com>
2022-04-28 11:25:50 +0200

    image, touchables, alerts

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("text pressed");
+  // console.log(require('./assets/icon.png'))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello World! Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's standard dummy
+      </Text>
+      <TouchableOpacity onPress={() => console.log("image tapped!")}>
+        <Image blurRadius={1} source={require("./assets/favicon.png")} />
+      </TouchableOpacity>
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() =>
+          // Alert.alert("Hey!", "How are you?", [
+          //   { text: "Good", onPress: () => console.log("Good Pressed") },
+          //   { text: "Bad", onPress: () => console.log("Bad Pressed") },
+          // ])
+          Alert.prompt('Tell me something funny', 'Message', msg => console.log(msg) )
+        }
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
